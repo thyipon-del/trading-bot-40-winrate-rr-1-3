79c714410b93c69537f4f9527208ec85ee8d08ed
thyipon-del <thyipon@gmail.com>
2026-09-13 00:37:25 +0700

    Add strategies package init

---
+++ strategies/__init__.py
@@ -0,0 +1,8 @@
+"""
+Trading Strategies Package
+Internal Flow Strategy - 40% Win Rate + 1:3 Risk/Reward Ratio
+"""
+
+from .internal_flow_strategy import InternalFlowStrategy
+
+__all__ = ['InternalFlowStrategy']
